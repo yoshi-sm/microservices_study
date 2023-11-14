ba1d85876359a8901dc59ec377938f917ed9974f
yoshi-sm <yoshismiranda@gmail.com>
2023-11-14 01:33:47 -0300

    httpdelete

--- AuctionService/Controllers/AuctionsController.cs
+++ AuctionService/Controllers/AuctionsController.cs
@@ -83,5 +83,22 @@ namespace AuctionService.Controllers
             return BadRequest("Problem saving changes.");
         }
 
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteAuction(Guid id)
+        {
+            var auction = await _context.Auctions.FindAsync(id);
+
+            if (auction == null) return NotFound();
+
+            //TODO: check seller == username
+
+            _context.Auctions.Remove(auction);
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if (!result) return BadRequest("Could not update database.");
+
+            return Ok(); 
+        }
     }
 }
