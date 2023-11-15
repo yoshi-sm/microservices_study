4287bb6eeaf99629afc3aa29dd96c14895cda3b4
yoshi-sm <yoshismiranda@gmail.com>
2023-11-15 19:24:14 -0300

    Seed data + item model on SearchService

--- SearchService/Program.cs
+++ SearchService/Program.cs
@@ -1,3 +1,8 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Data;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,5 +16,14 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+try
+{
+    await DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
 
 app.Run();
