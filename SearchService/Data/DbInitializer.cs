d5110d6c12f5abc8f5fac980c1324485e46cadb4
yoshi-sm <yoshismiranda@gmail.com>
2023-11-20 06:04:43 -0300

    Http communication between SearchService and AuctionService

--- SearchService/Data/DbInitializer.cs
+++ SearchService/Data/DbInitializer.cs
@@ -1,7 +1,7 @@
 ﻿using MongoDB.Driver;
 using MongoDB.Entities;
 using SearchService.Models;
-using System.Text.Json;
+using SearchService.Services;
 
 namespace SearchService.Data
 {
@@ -20,17 +20,15 @@ namespace SearchService.Data
 
             var count = await DB.CountAsync<Item>();
 
-            if (count == 0)
-            {
-                Console.WriteLine("No data, trying to seed.");
-                var itemData = await File.ReadAllTextAsync("Data/auctions.json");
+            using var scope = app.Services.CreateScope();
 
-                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
 
-                var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+            var items = await httpClient.GetItemsForSearchDb();
 
-                await DB.SaveAsync(items);
-            }
+            Console.WriteLine(items.Count + "returned from the auction service");
+
+            if (items.Count > 0) await DB.SaveAsync(items);
         }
     }
 }
